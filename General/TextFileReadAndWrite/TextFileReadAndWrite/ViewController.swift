dda2bfff93d6126521869c3da2cb11d9d4bbec20
Jing Wang <jing@figur8.me>
2019-02-11 16:48:56 -0500

    The app is able to read TextSample.txt from Bundle folder

--- General/TextFileReadAndWrite/TextFileReadAndWrite/ViewController.swift
+++ General/TextFileReadAndWrite/TextFileReadAndWrite/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var savedString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Path = \(FileManager.documentDirectoryURL.path)")
+        print("Main bundle = \(Bundle.main.bundlePath)")
+        
+        let bundleURL = Bundle.main.bundleURL
+        let resourceURL = Bundle.main.resourceURL
+        
+        print("mainBundleURL = \(bundleURL)")
+        print("resourceURL = \(resourceURL)")
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        do {
+            if let savedString = try read("TextSample", fileExtension: "txt", from: bundleURL) {
+                self.savedString = savedString
+                print("Saved String = \(self.savedString)")
+            }
+        } catch {
+            print("Error reading file! >> \(error)")
+        }
+        
+        
     }
-
-
+    
+    
+    
+    private func read(_ fileName: String, fileExtension: String = "", from url: URL = FileManager.documentDirectoryURL) throws -> String? {
+        var fileURL = url.appendingPathComponent(fileName)
+        if !fileExtension.isEmpty {
+            fileURL = fileURL.appendingPathExtension(fileExtension)
+        }
+        
+        let savedString = try String(contentsOf: fileURL, encoding: .utf8)
+        //        let savedString = try String(contentsOfFile: fileURL.path, encoding: .utf8)
+        //        let savedString = try String(contentsOfFile: fileURL.path, encoding: String.Encoding.init(rawValue: 4))
+        return savedString
+    }
+    
 }
 
+
+public extension FileManager {
+    static var documentDirectoryURL: URL {
+        return `default`.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+}
