d023fe108dcd0c4d89c3b70d9464c332cf2f9b1c
Jing Wang <jing@figur8.me>
2019-02-12 15:11:47 -0500

    Test completed and succeeded, stage #1,
     Verify .strings exist/not exit for supported/unsupported languages

--- General/TextFileReadAndWrite/TextFileReadAndWrite/ViewController.swift
+++ General/TextFileReadAndWrite/TextFileReadAndWrite/ViewController.swift
@@ -10,19 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var continueBtn: UIButton!
+    
     var savedString: String = ""
     var localDict = [String: String]()
+    var fileManager = FileManager.default
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("Path = \(FileManager.documentDirectoryURL.path)")
+        
+        continueBtn.setTitle(F8LocaleStrings.continueNext.localized, for: .normal)
+        
+        print("Supported languages >> \(Locale.preferredLanguages)")
+        
+        //        print("Path = \(FileManager.documentDirectoryURL.path)")
         print("Main bundle = \(Bundle.main.bundlePath)")
         
         let bundleURL = Bundle.main.bundleURL
         let resourceURL = Bundle.main.resourceURL
         
-//        print("mainBundleURL = \(bundleURL)")
-//        print("resourceURL = \(resourceURL)")
+        //        print("mainBundleURL = \(bundleURL)")
+        //        print("resourceURL = \(resourceURL)")
+        var localeFileURLList = [URL]()
+        
+        do {
+            localeFileURLList = try fileManager.contentsOfDirectory(at: Bundle.main.bundleURL, includingPropertiesForKeys: nil, options: [])
+        } catch {
+            print("Error in fetch localization files under path \(Bundle.main.bundlePath)")
+        }
+        
+        let bundleContents = localeFileURLList.map( {$0.lastPathComponent})
+        
+        print("Bundle Contents >> \(bundleContents)")
+        
+        var supportedLanguageList = bundleContents.filter( {$0.contains(".lproj") && !$0.contains("Base")})
+        supportedLanguageList = supportedLanguageList.map( {$0.replacingOccurrences(of: ".lproj", with: "")} )
+
+        print("Supported Languages >> \(supportedLanguageList)")
+        
         
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,31 +66,31 @@ class ViewController: UIViewController {
         if let fileName = Bundle.main.url(forResource: "TextSample", withExtension: "strings") {
             if let textNSDict = NSDictionary(contentsOf: fileName) {
                 if let textDict = textNSDict as? [String: String] {
-//                    print("textDict[\"doneText\"] = \(textDict["doneText"]))")
+                    //                    print("textDict[\"doneText\"] = \(textDict["doneText"]))")
                     localDict = textDict
                 }
-//                print("textNSDict[\"otherActivity\"] = \(textNSDict.object(forKey: "otherActivity"))")
+                //                print("textNSDict[\"otherActivity\"] = \(textNSDict.object(forKey: "otherActivity"))")
             }
         }
         
-        var outputText: String?
-        
-        for f8LocalKey in F8LocaleStrings.allCases {
-//            outputText = ""
-            outputText = nil
-            for localItem in localDict {
-                if localItem.key == f8LocalKey.rawValue {
-                    outputText = "Found match for key #\(f8LocalKey)# "
-                    if !localItem.value.isEmpty {
-                        outputText?.append("with value @\(f8LocalKey.rawValue)@")
-                        break
-                    }
-                }
-            }
-            print(outputText == nil ? "Not found match for key #\(f8LocalKey.rawValue)#": outputText!)
-        }
-        
-        
+//                var outputText: String?
+//
+//                for f8LocalKey in F8LocaleStrings.allCases {
+//        //            outputText = ""
+//                    outputText = nil
+//                    for localItem in localDict {
+//                        if localItem.key == f8LocalKey.rawValue {
+//                            outputText = "Found match for key #\(f8LocalKey)# "
+//                            if !localItem.value.isEmpty {
+//                                outputText?.append("with value @\(f8LocalKey.rawValue)@")
+//                                break
+//                            }
+//                        }
+//                    }
+//                    print(outputText == nil ? "Not found match for key #\(f8LocalKey.rawValue)#": outputText!)
+//                }
+//
+//
     }
     
     
--- General/TextFileReadAndWrite/TextFileReadAndWriteTests/TextFileReadAndWriteTests.swift
+++ General/TextFileReadAndWrite/TextFileReadAndWriteTests/TextFileReadAndWriteTests.swift
@@ -5,30 +5,78 @@
 //  Created by Jing Wang on 2/11/19.
 //  Copyright © 2019 Jing Wang. All rights reserved.
 //
-
+//import Foundation
 import XCTest
+
 @testable import TextFileReadAndWrite
 
 class TextFileReadAndWriteTests: XCTestCase {
-
+    
+    let expectedSupportedLangNameList: [String] = ["en", "zh-Hans"]
+    let testUnsupportedLangNameList: [String] = ["zh-Hant", "es", "ru"]
+    
+    var fileManager = FileManager.default
+    var bundleContentURLs = [URL]()
+    var supportedLangNameList = [String]()
+    var supportedLangURLs = [URL]()
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Refer to the following two links for iso language code
+        // https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+        // https://en.wikipedia.org/wiki/ISO_15924
+        
+        
+        do {
+            bundleContentURLs = try fileManager.contentsOfDirectory(at: Bundle.main.bundleURL, includingPropertiesForKeys: nil, options: [])
+        } catch {
+            print("Error in fetch localization files under path \(Bundle.main.bundlePath)")
+        }
+        
+        // Get URLs of all language package under main bundle i.e. [APP_NAME].APP/
+        supportedLangURLs = bundleContentURLs.filter( {$0.lastPathComponent.contains(".lproj") && !$0.lastPathComponent.contains("Base")} )
+        // Get names of all components under main bundle i.e. [APP_NAME].APP/
+        let bundleContentList = bundleContentURLs.map( {$0.lastPathComponent})
+        // Search for names of all language packages, which ends with ".lproj". Also remove name for base language package
+        supportedLangNameList = bundleContentList.filter( {$0.contains(".lproj") && !$0.contains("Base")} )
+        // Remove ".lproj" extension
+        supportedLangNameList = supportedLangNameList.map( {$0.replacingOccurrences(of: ".lproj", with: "")} )
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSupportedLanguages() {
+        // This is a test to veify whether the [LANUAGE_CODE].strings file for supported language [LANUAGE_CODE] should exist, test fails the corresponding file is not found
+        
+        for supportedLang in supportedLangNameList {
+            XCTAssertTrue(expectedSupportedLangNameList.contains(supportedLang), "Could not find localization file for supported language \(supportedLang)!")
+        }
     }
-
+    
+    
+    func testUnsupportedLanguages() {
+        // This is a test to veify whether the [LANUAGE_CODE].strings file for unsupported language [LANUAGE_CODE] should not exist, test fails the corresponding file is found
+        
+        for unsupportedLang in testUnsupportedLangNameList {
+            
+            XCTAssertFalse(supportedLangNameList.contains(unsupportedLang), "Do not support language \(unsupportedLang)!")
+        }
+    }
+    
+    
+    func testLocaleKeyValuePair() {
+        
+        for localeKey in F8LocaleStrings.allCases {
+            
+        }
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
